dcda5913986e7932cd8a7758e519b616da37ebc2
Carlos <carlosoc@live.com>
2021-01-01 20:42:09 -0600

    index.js modification

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 require('dotenv').config();
 const {dbConnection} =require('./database/config')
 const cors = require('cors');
-//crear el servidor de express
 
+//crear el servidor de express
 const app =express();
 
 //DataBase
 dbConnection();
 
 //CORS
-
 app.use(cors())
+
 //Directorio Publico
 app.use( express.static('public'));
 
@@ -20,8 +20,10 @@ app.use(express.json());
 
 //Rutas
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/events', require('./routes/events'))
+
 //TODO CRUD: Eventos
+app.use('/api/events', require('./routes/events'))
+
 
 //escuchar peticiones
 
